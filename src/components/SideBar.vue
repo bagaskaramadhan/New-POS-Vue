aefafbb49c307c91c18a0b51f6fa72ad35a017df
bagaskaramadhan <bagaskaramadhan@gmail.com>
2020-12-25 17:27:17 +0700

    change

---
+++ src/components/SideBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <img v-b-toggle.my-sidebar src="../assets/menu.png" width="50px" />
+    <b-sidebar id="my-sidebar" shadow width="110px">
+      <img src="../assets/menu.png" width="50px" />
+    </b-sidebar>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,23 @@
 <template>
   <div>
     <div class="row no-gutters text-center Navbar">
-      <div class="col-1 mt-3">Menu</div>
-      <div class="col-8 mt-3">Online Shop</div>
+      <div class="col-1 mt-3">
+        <SideBar/>
+      </div>
+      <div class="col-8 mt-3"><h1>Online Shop</h1></div>
       <div class="col mt-3">Checkout</div>
     </div>
     <div class="row no-gutters">
-      <div class="col-1"/>
+      <div class="col-1" />
       <div class="col-8"><Card /></div>
-      <div class="col"/>
+      <div class="col" />
     </div>
   </div>
 </template>
 
 <style scoped>
 .Navbar {
-  background: #FFFFFF;
+  background: #ffffff;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
   padding-bottom: 20px;
 }
@@ -23,9 +25,11 @@
 
 <script>
 import Card from '../components/Card'
+import SideBar from '../components/SideBar'
 export default {
   components: {
-    Card
+    Card,
+    SideBar
   }
 }
 </script>
